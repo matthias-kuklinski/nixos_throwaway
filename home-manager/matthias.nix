6057a67072b02993a38a71b7b2aa62d53d8caa80
Matthias Kuklinski <contact@matthias-kuklinski.com>
2024-04-16 13:48:11 +0200

    ts

--- home-manager/matthias.nix
+++ home-manager/matthias.nix
@@ -27,6 +27,20 @@ in
     "zsh"
   ];
 
+  {
+    home.sessionPath = [ "$HOME/.local/bin" ];
+    home.file = {
+      "zwift.sh" = {
+        source = pkgs.fetchurl {
+          url = "https://raw.githubusercontent.com/netbrain/zwift/master/zwift.sh";
+          hash = "sha256-joipzHtLvy+l4H+NOLTSpVf8bzVGUF4JVDcyfQIt5II=";
+        };
+        target = ".local/bin/zwift";
+        executable = true;
+      };
+    };
+  }
+
   programs.home-manager.enable = true;
 
   home = { 
